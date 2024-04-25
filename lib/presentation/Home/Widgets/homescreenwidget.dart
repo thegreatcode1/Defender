bf1e7f7c68c55b6968673823fe09b9d051b0c6dd
thegreatcode1 <suhaibpa72@gmail.com>
2024-04-26 00:41:38 +0530

    validate the formfield input

--- lib/presentation/Home/Widgets/homescreenwidget.dart
+++ lib/presentation/Home/Widgets/homescreenwidget.dart
@@ -5,7 +5,10 @@ import 'package:phish_defender/core/constants.dart';
 import 'package:phish_defender/core/textstyle.dart';
 import 'package:phish_defender/presentation/Result_screen/result.dart';
 
-final textcontroller =TextEditingController();
+final textcontroller = TextEditingController();
+String newValueController = textcontroller.toString();
+final formKey = GlobalKey<FormState>();
+
 class HomeButtonandTextfield extends StatelessWidget {
   const HomeButtonandTextfield({
     super.key,
@@ -15,50 +18,72 @@ class HomeButtonandTextfield extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(15.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextFormField(
-            controller: textcontroller,
-            style: hometextstyletextfieldurl,
-            decoration: InputDecoration(
-              fillColor: ktransparent.withOpacity(0.2),
-              hintText: "paste url",
-              hintStyle: hometextstyletextfield,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                borderSide: const BorderSide(
-                  color: kgrey,
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: textcontroller,
+              validator: urlValidator,
+              style: hometextstyletextfieldurl,
+              decoration: InputDecoration(
+                fillColor: ktransparent.withOpacity(0.2),
+                hintText: "paste url",
+                hintStyle: hometextstyletextfield,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: const BorderSide(
+                    color: kgrey,
+                  ),
                 ),
               ),
             ),
-          ),
-          khight,
-          MaterialButton(
-            onPressed: () {
-              String input = textcontroller.text;
-              Datadb.instance.getletterascii(input);
-              textcontroller.clear();
-              Navigator.of(context).pushNamed(ResultScreen.routename);
-            },
-            color: kwhite,
-            textColor: kblack,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(
-                  20.0,
+            khight,
+            MaterialButton(
+              onPressed: () {
+                //print(formKey.currentState!.validate());
+
+                String input = textcontroller.text;
+                if (formKey.currentState!.validate()) {
+                  Datadb.instance.getletterascii(input);
+                  textcontroller.clear();
+                  Navigator.of(context).pushNamed(ResultScreen.routename);
+                }
+              },
+              color: kwhite,
+              textColor: kblack,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(
+                    20.0,
+                  ),
                 ),
               ),
+              minWidth: 150,
+              height: 40,
+              child: const Text(
+                "Submit",
+                style: hometextstylebutton,
+              ),
             ),
-            minWidth: 150,
-            height: 40,
-            child: const Text(
-              "Submit",
-              style: hometextstylebutton,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  String? urlValidator(String? value) {
+    var urlPattern =
+        r"(https?|http)://([-A-Z0-9.]+\.?)+(:\d+)?(/[-A-Z0-9+&@#/%=~_|!:,.;]*?)?(\?[A-Z0-9+&@#/%=~_|!:.;]*)?";
+    if (value == null || value.isEmpty) {
+      return "enter valid url";
+    } else {
+       if (RegExp(urlPattern, caseSensitive: false).hasMatch(value)) {
+        return null; // URL is valid
+      } else {
+        return "Please enter a valid URL"; // Error message
+      }
+    }
   }
 }
