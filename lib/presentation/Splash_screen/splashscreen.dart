388c4fdc60d23f5f285e42fdf8353cb40c5f1897
thegreatcode1 <suhaibpa72@gmail.com>
2024-05-02 12:21:56 +0530

    add app icon on splash screen

--- lib/presentation/Splash_screen/splashscreen.dart
+++ lib/presentation/Splash_screen/splashscreen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:phish_defender/core/colors.dart';
+import 'package:phish_defender/core/constants.dart';
 import 'package:phish_defender/core/textstyle.dart';
 import 'package:phish_defender/presentation/Home/home.dart';
 
@@ -29,14 +30,30 @@ class SplashScreen extends StatelessWidget {
       child: Scaffold(
           backgroundColor: ktransparent,
           body: Center(
-            child: AnimatedTextKit(animatedTexts: [
-              ColorizeAnimatedText(
-                'DEFENDER',
-                textStyle: colorizeTextStyle,
-                colors: colorizeColors,
-                speed: const Duration(milliseconds: 400),
-              ),
-            ]),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 150,
+                  width: 150,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("lib/images/app_logo_scr.png"),
+                    ),
+                  ),
+                ),
+                khight,
+                khight,
+                AnimatedTextKit(animatedTexts: [
+                  ColorizeAnimatedText(
+                    'DEFENDER',
+                    textStyle: colorizeTextStyle,
+                    colors: colorizeColors,
+                    speed: const Duration(milliseconds: 400),
+                  ),
+                ]),
+              ],
+            ),
           )),
     );
   }
